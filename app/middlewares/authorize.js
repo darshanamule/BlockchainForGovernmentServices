0079d6a200798e9f0153c5d010c5f9abb9c06fde
darshana <darshanamule2002@gmail.com>
2022-02-20 11:16:31 +0530

    get forms update

--- app/middlewares/authorize.js
+++ app/middlewares/authorize.js
@@ -5,18 +5,18 @@ async function isAuthorized(req, res, next) {
 
         let to_address;
 
-        console.log('in auth')
+        // console.log('in auth')
             if(!req.params.id){
-                console.log('in params')
+
                 to_address = req.headers.auth;
             } else {
-                console.log('else params') 
+                // console.log('else params') 
                 to_address = req.params.id;
             }
 
             const from_address = req.headers.auth;
-            console.log(from_address)
-            console.log(to_address)
+            // console.log(from_address)
+            // console.log(to_address)
             const govStatus = await GovDetContract.methods.checkViewRights(to_address).call({ from: from_address })
             const bankStatus = await BankContract.methods.checkViewRights(to_address).call({ from: from_address })
             const busStatus = await BusinessContract.methods.checkViewRights(to_address).call({ from: from_address })
